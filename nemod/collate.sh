3cc177cf8376d47b7db56d26e2f088bc886dd24b
Tom Young <streamwork@twyoung.com>
2019-05-22 13:04:10 -0400

    Added options to sw.sh: Generate and view collate.jps

--- nemod/collate.sh
+++ nemod/collate.sh
@@ -2,10 +2,40 @@
 
 # COLLATE.sh
 
-	./sw */collate.sw
-	[ -d /dev/shm/sw ] || mkdir /dev/shm/sw
-	echo "(Sink strings.Print1)0 <- 0(Source strings.Gen1); " | ./sw > /dev/shm/sw/main.go
-	
-	pushd /dev/shm/sw/
+RunCollate() {
+	temp=/tmp
+	[ -d $temp/sw ] || mkdir $temp/sw
+	echo "(Si strings.Print1)0 <- 0(Source strings.Gen1); " | ./sw >  $temp/sw/main.go
+	pushd $temp/sw
 	[ -f go.mod ] || go mod init collate/collate
 	go run main.go 	
+}
+
+		# Create collate.jpg 		
+GenJPG() {  
+	make jpeg
+	
+	#./sw */coll* 1 > /tmp/collate_SW.dot 
+	#dot -Tjpg  /tmp/collate_SW.dot > /tmp/collate_SW.JPG
+	#gimp /tmp/collate_SW.JPG
+}	
+
+case $1 in	
+	j) GenJPG;;
+	r) RunCollate;;
+	x) $EDITOR collate.sw;;
+	*) cat << EOF 
+	
+USAGE: 
+		j		. Generate collate jpeg 
+		r		. Build and run Collate program (BUGGY)
+		x		. Edit this script
+		--help	. Display this help
+		
+EOF
+	;;	
+					
+esac	 
+	
+	
+
--- sw.sh
+++ sw.sh
@@ -1,4 +1,61 @@
+#!/bin/bash
 
+scr=sw.sh	
 
-bnfc --c $* SW.cf
+Die() {
+	echo "$scr/DIE: $*"
+	exit 1
+}
+
+ToDO() {
+	cat <<- EOF 
+	
+	#SW/ToDo:
+	 . Fix Go channels  
+	 . Color Go processes and edges green in Dot 
+	 . Switch to GoLand
+	 . Commit this script 
+	 . Generate Go/Component skeleton code
+	 . Generate JavaFBP network definitions  [BRANCH: Generate_Graph_Input]
+	 . Build C programs                      [BRANCH: Generate_C_code]
+	 
+EOF
+}
+
+# curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+BuildModel() {
+	pushd nemod || Die xx
+	# Die Cannot cd to ./nemod
+	[ -f sw.c ] || Die Missing sw.c 
+	[ -f Makefile ] || Die Missing ../nemod/Makefile 
+	make all
+}
+
+BuildIt() {
+	if [ -f Makefile ]
+		then bnfc --c    $* SW.cf && make && BuildModel
+	else		   
+		bnfc --c -m $* SW.cf && make && BuildModel
+	fi	
+}
+
+case $1 in
+	b)BuildIt;;
+	j)cd nemod; ./collate.sh j;;
+	poc) cd nemod; ./collate.sh r;;
+	v)gimp /tmp/collate.jpg;;
+	x)$EDITOR $scr;;
+	*)cat << EOF
+	
+USAGE:  $scr
+	b		. Build sw from SW.cf
+	j		. Generate and view /tmp/collate.jpg 
+	poc 		. Run PoC (Build and run simplistic Go program)  
+	v		. View collate.jpg
+	x		. Edit this script
+	--help		. Show this usage 
+	   		
+EOF
+	;;
+esac
 
