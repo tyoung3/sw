33189a672a5911bf64d566d10739b9fcce9b23f7
tyoung3 <githb@twyoung.com>
2021-12-04 14:02:46 -0500

    Created windows/bnfc

---
+++ windows/bnfc/Bison.h
@@ -0,0 +1,154 @@
+/* A Bison parser, made by GNU Bison 2.4.2.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2006, 2009-2010 Free Software
+   Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     _ERROR_ = 258,
+     _LPAREN = 259,
+     _RPAREN = 260,
+     _COMMA = 261,
+     _MINUS = 262,
+     _SYMB_13 = 263,
+     _DOT = 264,
+     _SLASH = 265,
+     _COLON = 266,
+     _SEMI = 267,
+     _LT = 268,
+     _EQ = 269,
+     _GT = 270,
+     _KW_INCLUDE = 271,
+     _KW_PREFIX = 272,
+     _SYMB_12 = 273,
+     _UNDERSCORE = 274,
+     _KW_include = 275,
+     _KW_prefix = 276,
+     _LBRACE = 277,
+     _RBRACE = 278,
+     T_Date = 279,
+     T_Envar = 280,
+     T_Id = 281,
+     T_Numvar = 282,
+     T_Stringvar = 283,
+     T_SubId = 284,
+     T_Symvar = 285,
+     T_ValidImport = 286,
+     _STRING_ = 287,
+     _INTEGER_ = 288
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1685 of yacc.c  */
+#line 116 "SW.y"
+
+  int    _int;
+  char   _char;
+  double _double;
+  char*  _string;
+  Valide valide_;
+  ValidSW validsw_;
+  Stm stm_;
+  ListStm liststm_;
+  Subdef subdef_;
+  Subnet subnet_;
+  ListSubnet listsubnet_;
+  ExtPortIn extportin_;
+  ExtPortOut extportout_;
+  Tab tab_;
+  DataFlow dataflow_;
+  Larrow larrow_;
+  Rarrow rarrow_;
+  TypeDef typedef_;
+  Buffsize buffsize_;
+  Hermt hermt_;
+  Symvalu symvalu_;
+  Proc proc_;
+  Prt prt_;
+  Comp comp_;
+  ModPath modpath_;
+  RemPath rempath_;
+  Argument argument_;
+  ListArgument listargument_;
+  Numassgn numassgn_;
+  Strassgn strassgn_;
+  SymAssgn symassgn_;
+  Numval numval_;
+  Stringval stringval_;
+  Symval symval_;
+  Include include_;
+  Prefix prefix_;
+  ValidConfig validconfig_;
+  Centry centry_;
+  ListCentry listcentry_;
+  Entry entry_;
+  ListEntry listentry_;
+  KeyVal keyval_;
+  KeyName keyname_;
+
+
+
+/* Line 1685 of yacc.c  */
+#line 132 "Bison.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+} YYLTYPE;
+# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+
+
