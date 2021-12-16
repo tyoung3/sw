c199d19baf1f2e0ba73879344f93755c6097143d
Tom Young <streamwork@twyoung.com>
2021-12-16 06:58:26 -0500

    Refactored

--- doc.go
+++ doc.go
@@ -1,8 +1,11 @@
-/*Package swbase contains code to launch Streamwork generated Go programs
-sw creates a Go program from a Streamwork network definition.  The
-generated Go  program uses this package to launch the components specified
-in the  network definition.
-Swbase contains basic Streamwork components: Gen, Pass, Print, etc.
-Other sw/... packages are: swutility(Collate,Merge) and linux(Wrap)
+/*Package sw contains the basic FBP engine to launch a network of Streamwork Go components.
+
+The program 'sw' can create a main/Go program(cmd) from a Streamwork network definition. 
+ 
+The generated cmd uses this package to launch the components specified in the  network definition.
+This package also contains basic Streamwork components: Gen, Pass, Print, etc.
+Other sw/... components are: swutility/Collate,Merge and linux/Wrap
+
+Sw components have a very simple interface.  There are no required import statements and just one exported function, with the Component name, is required.   
 */
 package sw
