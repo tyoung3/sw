3ecf43dc6b94c21f713b06b43b6be5f644944c7d
Tom Young <streamwork@twyoung.com>
2021-12-17 07:59:25 -0500

    Removed docs from .ac

---
+++ c/sw-0.18.4/model/swgo.h
@@ -0,0 +1,14 @@
+/** \file swgo.h
+
+ ** Streamwork swgo prototypes
+
+*/
+#ifndef SWGO_H_
+#define SWGO_H_
+
+#include "model.h"
+
+void genGo(Model mod);
+void genND(Model mod);   /* Generate Network Definition */
+
+#endif  // SWGO_H_
