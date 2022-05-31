41db8d00e4ac9faeb9c2b0aa813cb3bdb1331221
Tom <githb@twyoung.com>
2022-05-31 10:01:20 -0400

    Removed testtn(load balance)

--- testrtn/internal/gen_swproject.sh
+++
@@ -1,4 +0,0 @@
-swgen.sh gs swbal github.com/tyoung3/sw/gofbp YAML 1 0 Receiver 
-swgen.sh gs swbal github.com/tyoung3/sw/gofbp NONE 1 0 DelayedReceiver&
-swgen.sh gs swbal github.com/tyoung3/sw/gofbp NONE 1 3 LoadBalance&
-swgen.sh gs swbal github.com/tyoung3/sw/gofbp NONE 0 1 Sender Sender COUNT=10&
--- testrtn/internal/go.mod
+++
@@ -1,5 +0,0 @@
-module main
-
-go 1.16
-
-require github.com/tyoung3/sw v0.20.3
